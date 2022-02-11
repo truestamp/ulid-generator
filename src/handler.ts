fb6c0707cacf5618e22f67d434c1bbe9bf1cc8b0
Glenn Rempe <glenn@truestamp.com>
2022-02-10 23:22:14 -0500

    Update to new ulid-workers API
    Pull ulid functions inside the even handler req/res cycle
    Improve handling of params.
    Improve tests.

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
-import { ulid, monotonicFactory } from 'ulid-workers'
-const ulidm = monotonicFactory()
+import { ulidFactory, decodeTime } from 'ulid-workers'
+
+const TIME_MAX = Math.pow(2, 48) - 1
 
 export async function handleRequest(request: Request): Promise<Response> {
   const { searchParams } = new URL(request.url)
@@ -7,34 +8,60 @@ export async function handleRequest(request: Request): Promise<Response> {
   // q is the query parameter the represents how many ulids to generate "?q=10"
   const quantity = parseInt(searchParams.get('q') || '1', 10)
   if (isNaN(quantity)) {
-    return new Response(`Invalid q parameter (NaN): ${quantity}`, { status: 400 })
+    return new Response(`Invalid q parameter (NaN): ${quantity}`, {
+      status: 400,
+    })
   }
   if (quantity < 1) {
-    return new Response(`Invalid q parameter (<1): ${quantity}`, { status: 400 })
+    return new Response(`Invalid q parameter (<1): ${quantity}`, {
+      status: 400,
+    })
   }
   if (quantity > 1000) {
-    return new Response(`Invalid q parameter (>1000): ${quantity}`, { status: 400 })
+    return new Response(`Invalid q parameter (>1000): ${quantity}`, {
+      status: 400,
+    })
   }
 
   // m is the query parameter the represents whether to use the monotonic factory "?q=10&m=false"
   // setting m to 'false' will not use the monotonic factory, any other value will use it by default
-  let useMonotonic = searchParams.get('m') === "false" ? false : true
+  const useMonotonic = searchParams.get('m') === 'false' ? false : true
 
-  // seed is the query parameter the represents the seed to use for the ulid "?q=10&s=12345"
-  let seed
-  if (searchParams.get('s') !== null) {
-    seed = parseInt(searchParams.get('s') as string, 10)
-    // If the seed is valid we cannot use the monotonic factory
-    // as its function signature won't accept a seed.
-    useMonotonic = false
+  // 't' is the query parameter that represents the UNIX Epoch timestamp to use for the ulid "?q=10&t=12345"
+  let timestamp
+  if (searchParams.get('t') !== null) {
+    timestamp = parseInt(searchParams.get('t') as string, 10)
   }
+  if (timestamp && isNaN(timestamp)) {
+    return new Response(`Invalid t parameter (NaN): ${timestamp}`, {
+      status: 400,
+    })
+  }
+  if (timestamp && timestamp < 0) {
+    return new Response(`Invalid t parameter (<0): ${timestamp}`, {
+      status: 400,
+    })
+  }
+  if (timestamp && timestamp > TIME_MAX) {
+    return new Response(`Invalid t parameter (> 2^48 - 1): ${quantity}`, {
+      status: 400,
+    })
+  }
+
+  const ulidMonotonic = ulidFactory({ monotonic: true })
+  const ulidNonMonotonic = ulidFactory({ monotonic: false })
 
   const ulids = []
   for (let i = 0; i < quantity; i++) {
+    const ulid = useMonotonic
+      ? ulidMonotonic(timestamp)
+      : ulidNonMonotonic(timestamp)
+
     ulids.push({
       monotonic: useMonotonic,
-      ts: new Date().toISOString(),
-      ulid: useMonotonic ? ulidm() : ulid(seed),
+      t: decodeTime(ulid),
+      ts: new Date(decodeTime(ulid)).toISOString(),
+      ulid: ulid,
     })
   }
 
--- test/handler.test.ts
+++ test/handler.test.ts
@@ -1,10 +1,14 @@
 import { handleRequest } from '../src/handler'
 import makeServiceWorkerEnv from 'service-worker-mock'
+import { decodeTime } from 'ulid-workers'
 
 // node crypto for testing
-import crypto from 'crypto';
+import crypto from 'crypto'
 
 declare let global: any
+
+// const TEST_ULID = '01ARYZ6S41TSV4RRFFQ69G5FAV'
+const TEST_TIME_EPOCH_MS = 1469918176385
 
 describe('handle', () => {
   beforeAll(function () {
@@ -13,14 +17,14 @@ describe('handle', () => {
     global.crypto = {
       getRandomValues: function (buf: Uint8Array) {
         if (!(buf instanceof Uint8Array)) {
-          throw new TypeError("expected Uint8Array");
+          throw new TypeError('expected Uint8Array')
         }
-        const bytes = crypto.randomBytes(buf.length);
-        buf.set(bytes);
-        return buf;
-      }
-    };
-  });
+        const bytes = crypto.randomBytes(buf.length)
+        buf.set(bytes)
+        return buf
+      },
+    }
+  })
 
   beforeEach(() => {
     Object.assign(global, makeServiceWorkerEnv())
@@ -52,17 +56,23 @@ describe('handle', () => {
   })
 
   test('returns an error with q=1001', async () => {
-    const result = await handleRequest(new Request('/?q=1001', { method: 'GET' }))
+    const result = await handleRequest(
+      new Request('/?q=1001', { method: 'GET' }),
+    )
     expect(result.status).toEqual(400)
   })
 
   test('returns an error with q=foo', async () => {
-    const result = await handleRequest(new Request('/?q=foo', { method: 'GET' }))
+    const result = await handleRequest(
+      new Request('/?q=foo', { method: 'GET' }),
+    )
     expect(result.status).toEqual(400)
   })
 
-  test('returns a non-monotonicFactory ULID only if requested', async () => {
-    const result = await handleRequest(new Request('/?m=false', { method: 'GET' }))
+  test('returns a non-monotonic ULID only if requested', async () => {
+    const result = await handleRequest(
+      new Request('/?m=false', { method: 'GET' }),
+    )
     expect(result.status).toEqual(200)
     const text = await result.text()
     const obj = JSON.parse(text)
@@ -70,8 +80,10 @@ describe('handle', () => {
     expect(obj[0]).toHaveProperty('monotonic', false)
   })
 
-  test('returns a monotonicFactory ULID for any non "false" value of m', async () => {
-    const result = await handleRequest(new Request('/?m=true', { method: 'GET' }))
+  test('returns a monotonic ULID for any non "false" value of m', async () => {
+    const result = await handleRequest(
+      new Request('/?m=true', { method: 'GET' }),
+    )
     expect(result.status).toEqual(200)
     const text = await result.text()
     const obj = JSON.parse(text)
@@ -79,16 +91,36 @@ describe('handle', () => {
     expect(obj[0]).toHaveProperty('monotonic', true)
   })
 
-  test('returns a specified time ULID with a valid seed value', async () => {
-    const result = await handleRequest(new Request('/?s=1469918176385', { method: 'GET' }))
+  test('returns a single non-monotonic ULID with a specified timestamp value', async () => {
+    const result = await handleRequest(
+      new Request(`/?q=1&t=${TEST_TIME_EPOCH_MS}&m=false`, { method: 'GET' }),
+    )
     expect(result.status).toEqual(200)
     const text = await result.text()
     const obj = JSON.parse(text)
+    // console.log(obj)
     expect(obj).toHaveLength(1)
-    // Monotonic function is not used when a seed is specified
     expect(obj[0]).toHaveProperty('monotonic', false)
-    // The timestamp portion of the ULID is derived from the seed value
-    expect(obj[0].ulid).toContain('01ARYZ6S41')
+    expect(obj[0]).toHaveProperty('t', TEST_TIME_EPOCH_MS)
+    expect(decodeTime(obj[0].ulid)).toBe(TEST_TIME_EPOCH_MS)
   })
 
+  test('returns multiple monotonic ULIDs with a specified timestamp value', async () => {
+    const result = await handleRequest(
+      new Request(`/?q=10&t=${TEST_TIME_EPOCH_MS}`, { method: 'GET' }),
+    )
+    expect(result.status).toEqual(200)
+    const text = await result.text()
+    const obj = JSON.parse(text)
+    // console.log(obj)
+    expect(obj).toHaveLength(10)
+
+    expect(obj[0]).toHaveProperty('monotonic', true)
+    expect(obj[0]).toHaveProperty('t', TEST_TIME_EPOCH_MS)
+    expect(decodeTime(obj[0].ulid)).toBe(TEST_TIME_EPOCH_MS)
+
+    expect(obj[1]).toHaveProperty('monotonic', true)
+    expect(obj[1]).toHaveProperty('t', TEST_TIME_EPOCH_MS)
+    expect(decodeTime(obj[1].ulid)).toBe(TEST_TIME_EPOCH_MS)
+  })
 })
